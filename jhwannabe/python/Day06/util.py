9f309766ccd6e01a10fbb8240bd6727084b526c2
jhwannabe <jhjun0328@naver.com>
2023-03-10 14:44:37 +0900

    3/10 강의내용

--- jhwannabe/python/Day06/util.py
+++ jhwannabe/python/Day06/util.py
@@ -9,5 +9,14 @@ def total(start, end):
 def calc(num1, num2):
     return num1 + num2, num1 - num2, num1 * num2, num1 / num2
 
-# print(total(1, 100))
-# print(calc(10, 3))
+'''
+__name__
+- 파이썬이 내부적으로 사용하는 특별한 변수 이름
+- .py 파일이 직접 실행될 경우 __name__ 변수에 __main__값이 자동으로 저장됨
+- 다른 파이썬 모듈에서 import하여 .py 파일을 사용할 경우 __name__에 모듈 이름 값이 저장됨
+'''
+
+
+if __name__ == '__main__':
+    print(total(1, 100))
+    print(calc(10, 3))
---
+++ jhwannabe/python/Day06/util2.py
@@ -0,0 +1,8 @@
+PI = 3.1415926535897932
+
+class Math:
+    def circle(self, radius):
+        return PI * (radius ** 2)
+    
+    def add(self, a,b):
+        return a + b
