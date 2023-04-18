0d832a109d6d24919c796561d22a498772c2d9e5
jhwannabe <jhjun0328@naver.com>
2023-04-18 09:04:10 +0900

    4/17 강의내용

---
+++ nodejs/Day32/5. module.js
@@ -0,0 +1,12 @@
+// import * as counter from './counter2.js'
+import { getCount, increase } from './counter2.js'
+
+// counter.increase();
+// counter.increase();
+// counter.increase();
+// console.log(counter.getCount());
+
+increase();
+increase();
+increase();
+console.log(getCount());
